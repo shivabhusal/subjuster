6679718de81ee0b35591ae785dba3bb3f7c3a077
Shiva Bhusal <hotline.shiva@gmail.com>
2017-06-29 22:04:19 +0545

    Production code written to pass test specs of user_input_spec.rb

--- lib/subjuster/user_input.rb
+++ lib/subjuster/user_input.rb
@@ -1,8 +1,14 @@
 module Subjuster
   class UserInput
-    attr_reader :source_filepath
-    def initialize(source:)
+    attr_reader :source_filepath, :target_filepath, :adjustment_in_sec
+    def initialize(source:, target: nil, adjustment_in_sec: 0)
       @source_filepath = source
+      @target_filepath = target || @source_filepath
+      @adjustment_in_sec = adjustment_in_sec
+    end
+    
+    def valid?
+      File.exist?(source_filepath)
     end
   end
 end
