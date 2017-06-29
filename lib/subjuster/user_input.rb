73fd23161054a98b51107d732aadf3aec36c94b5
Shiva Bhusal <hotline.shiva@gmail.com>
2017-06-29 22:21:40 +0545

    RDOC added in user_input.rb

--- lib/subjuster/user_input.rb
+++ lib/subjuster/user_input.rb
@@ -1,4 +1,14 @@
 module Subjuster
+  # = Handle User Input
+  # 
+  # Being a CLI tool, Subjuster is supposed to take input from +ARGV+
+  # This module will get those data via arguments to the constructor method.
+  #
+  # === For Example:
+  #
+  #   inputs = UserInput.new(source: ARGV[0], target: ARGV[1], adjustment_in_sec: ARGV[2])
+  #   inputs.valid? # => true / false
+  #
   class UserInput
     attr_reader :source_filepath, :target_filepath, :adjustment_in_sec
     def initialize(source:, target: nil, adjustment_in_sec: 0)
@@ -7,6 +17,18 @@ module Subjuster
       @adjustment_in_sec = adjustment_in_sec
     end
     
+    # Validates the source file, if it exists
+    # === Valid condition
+    # 
+    #   inputs = UserInput.new(source: ARGV[0], target: ARGV[1], adjustment_in_sec: ARGV[2])
+    #   inputs.valid? # => true 
+    #
+    # === Invalid condition
+    # 
+    #   inputs = UserInput.new(source: ARGV[0], target: ARGV[1], adjustment_in_sec: ARGV[2])
+    #   inputs.valid? # => false
+    #
+    #
     def valid?
       File.exist?(source_filepath)
     end
